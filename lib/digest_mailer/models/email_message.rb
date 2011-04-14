ffa3917438dfe046466dfb4e69abe8400d631721
Sean Zehnder <seanzehnder@gmail.com>
2011-04-14 15:30:31 -0400

    checking in the email_message (as struct) prior to refactoring it into an activerecord model

--- lib/digest_mailer/models/email_message.rb
+++ lib/digest_mailer/models/email_message.rb
@@ -1,20 +1,21 @@
 module DigestMailer
-  class EmailMessage < Struct.new(:recipient, :from_email, :message_params)
-    cattr_accessor :mailer_method, :from_email, :to, :body, :intended_sent_at, :from
+  class EmailMessage < Struct.new(:recipient, :from_email, :subject, :body, :message_params)
+    cattr_accessor :mailer_method, :from_email, :to, :body, :from, :subject, :intended_sent_at
 
     def perform
       @message_params = message_params      
       @mailer_method = @message_params[:mailer_method] ? @message_params[:mailer_method] : 'generic_message'
       @from_email = from_email
       @from = nil #Fixme
+      @subject = subject
+      @body = body
       @to = recipient
-      @body = "Fixme -- the body of this email message is not yet being set properly"
+      @intended_sent_at = Time.now
       send
     end
 
     # send the message to the queue for delivery
     def send
-      @intended_sent_at = Time.now
       MailDispatcher.send(@mailer_method, self)
       MailLogger.log(self)
     end
